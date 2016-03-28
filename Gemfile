4ec62dc7a6d84edb4bab542d83906687916aeb70
nnagato <darkdragon236@gmail.com>
2016-03-28 16:31:17 +0000

    finish copy previous version git

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'bootstrap-sass',       '3.2.0.0'
+gem 'bcrypt',               '3.1.7'
 
 group :development, :test do
   gem 'sqlite3',     '1.3.9'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,29 @@
 Rails.application.routes.draw do
+
+  get 'users/new'
+
+  get 'users/new'
+
+  get 'user/new'
+
+  get 'users/new'
+
+  get 'user/new'
+
+  get 'help' => 'static_pages#help'
+  
+  get 'about' => 'static_pages#about'
+  
+  get 'contact' => 'static_pages#contact'
+  
+  get 'signup' => 'user#new'
+  
+  #get 'static_pages/contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
