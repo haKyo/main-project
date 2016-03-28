3a3c001c5aceeee75dc5846fbfafd97b97640797
nnagato <darkdragon236@gmail.com>
2016-03-28 20:36:29 +0000

    finished chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
-  get 'users/new'
-
-  get 'user/new'
-
-  get 'users/new'
-
-  get 'user/new'
-
   get 'help' => 'static_pages#help'
-  
   get 'about' => 'static_pages#about'
-  
   get 'contact' => 'static_pages#contact'
-  
-  get 'signup' => 'user#new'
+  get 'signup' => 'users#new'
+  resources :users
   
   #get 'static_pages/contact'
   # The priority is based upon order of creation: first created -> highest priority.
